aed0930d4a425057385c5efd1c0ad3c72374fd99
sdmiller1 <sdmiller1@madisoncollege.edu>
2020-06-22 15:44:21 -0500

    made the resources directory publicly accessible

--- src/recipeTracker.js
+++ src/recipeTracker.js
@@ -2,6 +2,9 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
+// Make resources directory publicly available
+app.use('/resources', express.static(__dirname + '\\resources'));
+
 // Return an html file
 app.get("/", (request, response) => {
     const fileName = "index.html";
