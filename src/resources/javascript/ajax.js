39885a2e757115e7ca78a4672e0bc16c65060a06
sdmiller1 <sdmiller1@madisoncollege.edu>
2020-07-06 13:13:14 -0500

    implemented ajax for the view recipe page

--- src/resources/javascript/ajax.js
+++ src/resources/javascript/ajax.js
@@ -13,3 +13,19 @@ const getAllRecipes = (callback) => {
             callback(result);
         });
 }
+
+const getRecipeById = (id, callback) => {
+    let url = apiPath + "/" + id;
+    let parameters = {
+        "method": "get"
+    };
+
+    fetch(url, parameters)
+        .then(result => {
+            return result.json();
+        }).then(result => {
+            callback(result);
+        });
+}
+
+// Search For Recipe
---
+++ src/resources/javascript/view.js
@@ -0,0 +1,56 @@
+// JS to populate the page with the requested recipe
+
+const displayRecipe = () => {
+
+    let outputLocation = document.querySelector("#recipeContatiner");
+
+    // TODO: this is currently really fragile
+    let recipeId = parseInt(window.location.toString().split("/")[4]);
+
+    getRecipeById(recipeId, recipe => {
+
+        let recipeSection = `
+            <section class="row mt-3">
+                <div class="col-lg-6 col-md-6 col-6">
+                    <img src="/resources/images/${recipe.image}" alt="${recipe.name}" class="rounded-lg w-100">
+                </div>
+                <div class="col-lg-6 col-md-6 col-6">
+                    <h2>${recipe.name}</h2>
+                    <!-- TODO: finish implementing the rating js -->
+                    <div class="rating mt-2">
+                        <i class="fa fa-star-o" onclick="rateMovie(this)" data-rating="1"></i>
+                        <i class="fa fa-star-o" onclick="rateMovie(this)" data-rating="2"></i>
+                        <i class="fa fa-star-o" onclick="rateMovie(this)" data-rating="3"></i>
+                        <i class="fa fa-star-o" onclick="rateMovie(this)" data-rating="4"></i>
+                        <i class="fa fa-star-o" onclick="rateMovie(this)" data-rating="5"></i>
+                    </div>
+                    <p class="">
+                        ${recipe.description}
+                    </p>
+                </div>
+            </section>
+            <section class="row mt-3">
+                <h3 class="col-12">
+                    Ingredients:
+                </h3>
+                <div class="col-4 text-center mb-1">
+                    <label class="form-check-label">
+                        <input type="checkbox" class="form-check-input"> 2 Cups Flour
+                    </label>
+                </div>
+                <h3 class="col-12">
+                    Recipe:
+                </h3>
+                <p class="col-12">
+                    ${recipe.instructions}
+                </p>
+            </section>
+        `;
+
+        outputLocation.innerHTML = recipeSection;
+    });
+
+
+}
+
+window.onload = displayRecipe;
