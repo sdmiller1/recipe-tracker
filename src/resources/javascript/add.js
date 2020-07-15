70bb6f091dcd26362f8592165cc21f4b6c692fda
sdmiller1 <sdmiller1@madisoncollege.edu>
2020-07-15 13:15:28 -0500

    The add recipe function is now working
    it is still missing a couple of things such as the image upload

--- src/resources/javascript/add.js
+++ src/resources/javascript/add.js
@@ -1,5 +1,22 @@
 // JS used for adding a recipe
 
+const submitNewRecipe = () => {
+    let recipe = {};
+
+    recipe["title"] = document.querySelector("#recipeTitle").value;
+    recipe["description"] = document.querySelector("#recipeDescription").value;
+    recipe["instructions"] = document.querySelector("#recipeInstructions").value;
+    recipe["image"] = "pizza.jpg";
+    recipe["ingredients"] = [];
+
+    let ingredients = document.querySelectorAll(".recipeIngredient");
+    ingredients.forEach(node => {recipe["ingredients"].push(node.value)})
+
+
+    console.log(recipe);
+    addNewRecipe(recipe);
+}
+
 const addIngredient = () => {
     let ingredientsContainer = document.querySelector("#ingredientsContainer");
 
